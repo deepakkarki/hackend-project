82643c2c5741ab960c38d5f24067526104dd58ca
deepakkarki <deepak6k@gmail.com>
2015-08-24 23:03:06 +0530

    added skeleton logic. everything compiles and works

--- week-4/C-Python-API/adder.c
+++ week-4/C-Python-API/adder.c
@@ -1,2 +1,25 @@
 //The C file that uses the Python C api to add all numbers in a list
 
+#include <Python.h>
+
+static PyObject* addList_add(PyObject* self, PyObject* args){
+  //logic here
+  int sum = 0;
+  return Py_BuildValue("i", sum);
+}
+
+static char addList_docs[] =
+    "add( ): add all elements of the list\n";
+
+static PyMethodDef addList_funcs[] = {
+    {"add", (PyCFunction)addList_add, METH_VARARGS, addList_docs},
+    {NULL, NULL, 0, NULL}
+};
+
+/*
+addList is the module name
+*/
+PyMODINIT_FUNC initaddList(void){
+    Py_InitModule3("addList", addList_funcs,
+                   "Add all ze lists");
+}
